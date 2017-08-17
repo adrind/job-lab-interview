e3f2e2b190254553e99a7a11d6958724a46f948c
Adrienne <adrienne@codeforamerica.org>
2017-08-16 23:56:19 -0800

    added basic google analytics

--- interview_prep/static/js/interview_prep.js
+++ interview_prep/static/js/interview_prep.js
@@ -1,24 +1,27 @@
 $(function () {
    $('.btn-answer').click(function (evt) {
        var $el = $(evt.target),
-           text = $el.parent().find('.suggestion').text();
+           text = $el.parent().find('.suggestion').text(),
+           questionText = $('.question-text').text();
 
        if($el.data('is-correct') === 'True') {
            $('.incorrect').hide();
            $('.correct.suggestion-box').show().find('.suggestion-text').html(text);
            $('.btn-answer').addClass('disabled');
            $el.addClass('correct-answer');
+           ga('send', 'event', 'answer', 'correct', questionText, text);
        } else {
            $('.incorrect.suggestion-box').show().find('.suggestion-text').html(text);
            $('.incorrect').show();
+           ga('send', 'event', 'answer', 'wrong', questionText, text);
        }
-
    });
 
    $('.btn-repeat').click(function (evt) {
-       var $el = $(evt.target);
+       var $el = $(evt.target),
+           questionText = $('.question-text').text();
        $('.btn-answer').removeClass('disabled correct-answer');
-
        $('.suggestion-box').hide();
+       ga('send', 'event', 'repeat', 'correct', questionText);
    });
 });
